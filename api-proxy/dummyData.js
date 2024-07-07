f76aa6a64945ee01f4644b589e8dffe1d7d3e1af
Annalisa Comin <comin@cip.ifi.lmu.de>
2024-07-07 23:22:03 +0200

    added dummy data for backend

---
+++ api-proxy/dummyData.js
@@ -0,0 +1,18 @@
+module.exports = {
+  vitals: {
+    FactoryTimestamp: '2024-07-04T15:58:28Z',
+    Timestamp: '2024-07-04T17:58:28Z',
+    type: 1,
+    ValueInMgPerDl: 122,
+    TrendArrow: 3,
+    TrendMessage: null,
+    MeasurementColor: 1,
+    GlucoseUnits: 1,
+    Value: 122,
+    isHigh: false,
+    isLow: false,
+  },
+  openai: {
+    message: 'Here is a dummy response from OpenAI.',
+  },
+};
--- api-proxy/index.js
+++ api-proxy/index.js
@@ -4,8 +4,11 @@ const OpenAI = require('openai');
 const cors = require('cors');
 require('dotenv').config();
 
+const dummyData = require('./dummyData');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+const USE_DUMMY_DATA = process.env.USE_DUMMY_DATA ?? true;
 
 app.use(cors());
 app.use(express.json());
@@ -15,31 +18,40 @@ const openai = new OpenAI({
 });
 
 app.get('/vitals', async (req, res) => {
-  try {
-    const response = await axios.get(process.env.API_URL);
-    res.send(response.data);
-  } catch (error) {
-    console.error('Error fetching vitals:', error);
-    res.status(500).send('Error fetching vitals');
+  console.log(USE_DUMMY_DATA);
+  if (USE_DUMMY_DATA) {
+    res.send(dummyData.vitals);
+  } else {
+    try {
+      const response = await axios.get(process.env.API_URL);
+      res.send(response.data);
+    } catch (error) {
+      console.error('Error fetching vitals:', error);
+      res.status(500).send('Error fetching vitals');
+    }
   }
 });
 
 app.post('/openai', async (req, res) => {
-  try {
-    const { prompt } = req.body;
-    console.log('Prompt received:', prompt);
+  if (USE_DUMMY_DATA) {
+    res.send(dummyData.openai);
+  } else {
+    try {
+      const { prompt } = req.body;
+      console.log('Prompt received:', prompt);
 
-    const completion = await openai.chat.completions.create({
-      messages: [{ role: 'system', content: prompt }],
-      model: 'gpt-3.5-turbo-1106',
-      temperature: 1.5,
-    });
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: 'system', content: prompt }],
+        model: 'gpt-3.5-turbo-1106',
+        temperature: 1.5,
+      });
 
-    const message = completion.choices[0]?.message?.content || '';
-    res.send({ message });
-  } catch (error) {
-    console.error('Error generating completion:', error);
-    res.status(500).send({ error: true });
+      const message = completion.choices[0]?.message?.content || '';
+      res.send({ message });
+    } catch (error) {
+      console.error('Error generating completion:', error);
+      res.status(500).send({ error: true });
+    }
   }
 });
 
