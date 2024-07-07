d0976728f26c7f90e2dbf64f066a9523077dca14
Annalisa Comin <comin@cip.ifi.lmu.de>
2024-07-07 23:08:45 +0200

    refactoring

--- api-proxy/index.js
+++ api-proxy/index.js
@@ -1,41 +1,48 @@
-var express = require('express');
-var axios = require('axios');
-var app = express();
-var OpenAI = require('openai');
-var cors = require('cors');
+const express = require('express');
+const axios = require('axios');
+const OpenAI = require('openai');
+const cors = require('cors');
 require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-var openai = new OpenAI({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.get('/vitals', async function (req, res) {
+app.get('/vitals', async (req, res) => {
   try {
     const response = await axios.get(process.env.API_URL);
     res.send(response.data);
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching vitals:', error);
+    res.status(500).send('Error fetching vitals');
   }
 });
 
-app.post('/openai', async function (req, res) {
+app.post('/openai', async (req, res) => {
   try {
-    console.log(req.body);
+    const { prompt } = req.body;
+    console.log('Prompt received:', prompt);
+
     const completion = await openai.chat.completions.create({
-      messages: [{ role: 'system', content: req.body.prompt }],
+      messages: [{ role: 'system', content: prompt }],
       model: 'gpt-3.5-turbo-1106',
       temperature: 1.5,
     });
 
-    const message = completion.choices[0].message.content ?? '';
-    res.send({ message: message });
+    const message = completion.choices[0]?.message?.content || '';
+    res.send({ message });
   } catch (error) {
-    console.log(error);
-    res.send({ error: true });
+    console.error('Error generating completion:', error);
+    res.status(500).send({ error: true });
   }
 });
 
-var server = app.listen(5000);
+app.listen(PORT, () => {
+  console.log(`Api proxy is running on port ${PORT}`);
+});
