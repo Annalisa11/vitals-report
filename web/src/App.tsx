1d9d8ab7eef49a13ac17a3899e0ba6e96431451c
Annalisa Comin <comin@cip.ifi.lmu.de>
2024-07-08 22:37:05 +0200

    added chart basis

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,16 @@ import './App.scss';
 import Vitals from './components/Vitals';
 import Jokes from './components/Jokes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+import dummyData from './assets/chart-data.json';
 
 export type VitalsType = {
   FactoryTimestamp: string;
@@ -31,6 +41,43 @@ const App: React.FC = () => {
         <main>
           <Vitals />
           <Jokes />
+          huhu
+          <ResponsiveContainer width={'100%'} height={300}>
+            <AreaChart
+              width={500}
+              height={400}
+              data={dummyData}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <defs>
+                <linearGradient id='colorUv' x1='0' y1='0' x2='0' y2='1'>
+                  <stop offset='5%' stopColor='#82ca9d' stopOpacity={0.8} />
+                  <stop offset='95%' stopColor='#82ca9d' stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis
+                dataKey='Timestamp'
+                domain={['auto', 'auto']}
+                interval={'equidistantPreserveStart'}
+                tickCount={10}
+              />
+
+              <YAxis domain={['dataMin - 30', 'dataMax + 30']} />
+              <Tooltip />
+              <Area
+                type='monotone'
+                dataKey='ValueInMgPerDl'
+                stroke='#82ca9d'
+                fillOpacity={1}
+                fill='url(#colorUv)'
+              />
+            </AreaChart>
+          </ResponsiveContainer>
         </main>
       </div>
     </QueryClientProvider>
