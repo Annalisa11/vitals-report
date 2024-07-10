f0c58b382c99e07fefd851cbe65f137d1ffd337d
agaa-aaga <agatakim98@gmail.com>
2024-07-10 23:17:11 +0200

    Added backend project

---
+++ backend/Utils/DavidApiClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using backend.Models;
+
+namespace backend.Utils
+{
+    public class DavidApiClient
+    {
+        public static async Task<VitalsFromDavidDto> GetDavidVitalsAsync()
+        {
+            var httpClient = new HttpClient();
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://legendary-api.davidpenn.de/cgm?type=current");
+            var responseMessage = await httpClient.SendAsync(httpRequestMessage);
+            var responseBody = await responseMessage.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<VitalsFromDavidDto>(responseBody)!;
+        }
+    }
+}
