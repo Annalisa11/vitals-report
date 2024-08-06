5b45071af610ea80b6f30b29d3f2ef280e3dc1b1
Annalisa Comin <annalisa.comin@hotmail.com>
2024-08-06 14:14:58 +0200

    updated button with optional icon

--- web/src/components/basic/Button.tsx
+++ web/src/components/basic/Button.tsx
@@ -3,12 +3,28 @@ import './Button.scss';
 
 type Props = {
   variant?: 'primary' | 'secondary' | 'green' | 'delete';
+  icon?: React.ReactNode;
+  iconPosition?: 'left' | 'right';
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ variant = 'secondary', children, ...props }: Props) => {
+const Button = ({
+  variant = 'secondary',
+  children,
+  icon,
+  iconPosition = 'left',
+  ...props
+}: Props) => {
   return (
     <button className={`button ${variant}`} {...props}>
-      {children}
+      <div className='button__content'>
+        {icon && iconPosition === 'left' && (
+          <span className='icon left'>{icon}</span>
+        )}
+        {children}
+        {icon && iconPosition === 'right' && (
+          <span className='icon right'>{icon}</span>
+        )}
+      </div>
     </button>
   );
 };
