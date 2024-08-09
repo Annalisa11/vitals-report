f4a4d826bbd594d4691e7a83314696e40215a00d
Annalisa Comin <annalisa.comin@hotmail.com>
2024-08-09 21:33:32 +0200

    Fixed build errors

--- web/src/components/admin/UserRightsAccordionContent.tsx
+++ web/src/components/admin/UserRightsAccordionContent.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import * as Accordion from '@radix-ui/react-accordion';
 import axios from 'axios';
 import { BASE_URL } from '../../config';
