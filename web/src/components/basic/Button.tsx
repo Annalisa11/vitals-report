37ff05b22376fa71c166d9db7a3616b7ab5ab1d2
Annalisa Comin <annalisa.comin@hotmail.com>
2025-01-11 20:17:22 +0100

    fixed button ref error (hopefully)

--- web/src/components/basic/Button.tsx
+++ web/src/components/basic/Button.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { forwardRef, ButtonHTMLAttributes } from 'react';
 import './Button.scss';
 
 type Options = {
@@ -12,30 +12,35 @@ type Props = {
   options?: Options;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({
-  variant = 'secondary',
-  children,
-  icon,
-  iconPosition = 'left',
-  options = {},
-  ...props
-}: Props) => {
-  const { compact } = options;
-  const buttonClass = `button ${variant} ${compact ? 'compact' : ''}`;
+const Button = forwardRef<HTMLButtonElement, Props>(
+  (
+    {
+      variant = 'secondary',
+      children,
+      icon,
+      iconPosition = 'left',
+      options = {},
+      ...props
+    },
+    ref
+  ) => {
+    const { compact } = options;
+    const buttonClass = `button ${variant} ${compact ? 'compact' : ''}`;
 
-  return (
-    <button className={buttonClass} {...props}>
-      <div className='button__content'>
-        {icon && iconPosition === 'left' && (
-          <span className='icon left'>{icon}</span>
-        )}
-        {children}
-        {icon && iconPosition === 'right' && (
-          <span className='icon right'>{icon}</span>
-        )}
-      </div>
-    </button>
-  );
-};
+    return (
+      <button className={buttonClass} ref={ref} {...props}>
+        <div className='button__content'>
+          {icon && iconPosition === 'left' && (
+            <span className='icon left'>{icon}</span>
+          )}
+          {children}
+          {icon && iconPosition === 'right' && (
+            <span className='icon right'>{icon}</span>
+          )}
+        </div>
+      </button>
+    );
+  }
+);
 
 export default Button;
